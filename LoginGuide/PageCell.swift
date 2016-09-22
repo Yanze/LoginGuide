88342f23ae380a088bbce3403ba9597dd72dfdd5
Yanze LIU <yanzeliu.france@gmail.com>
2016-09-22 15:06:53 -0700

    page control

--- LoginGuide/PageCell.swift
+++ LoginGuide/PageCell.swift
@@ -18,12 +18,12 @@ class PageCell: UICollectionViewCell {
             imageView.image = UIImage(named: page.imageName)
             
             let color = UIColor(white: 0.2, alpha: 1)
-            let attributeText = NSMutableAttributedString(string: page.title, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(18, weight: UIFontWeightMedium), NSForegroundColorAttributeName: color])
+            let attributeText = NSMutableAttributedString(string: page.title, attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18, weight: UIFontWeightMedium), NSForegroundColorAttributeName: color])
             
-            attributeText.appendAttributedString(NSAttributedString(string: "\n\n\(page.message)", attributes: [NSFontAttributeName: UIFont.systemFontOfSize(14), NSForegroundColorAttributeName: color]))
+            attributeText.append(NSAttributedString(string: "\n\n\(page.message)", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 14), NSForegroundColorAttributeName: color]))
             
             let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = .Center
+            paragraphStyle.alignment = .center
             let length = attributeText.string.characters.count
             attributeText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: length))
             textView.attributedText = attributeText
@@ -41,7 +41,7 @@ class PageCell: UICollectionViewCell {
     
     let imageView: UIImageView = {
         let iv = UIImageView()
-        iv.contentMode = .ScaleAspectFill
+        iv.contentMode = .scaleAspectFill
         iv.image = UIImage(named: "1.jpg")
         iv.clipsToBounds = true
         return iv
@@ -50,7 +50,7 @@ class PageCell: UICollectionViewCell {
     let textView: UITextView = {
         let tv = UITextView()
         tv.text = "Some text here"
-        tv.editable = false
+        tv.isEditable = false
         tv.contentInset = UIEdgeInsets(top: 24, left: 0, bottom: 0, right: 0)
         return tv
     }()
@@ -69,8 +69,8 @@ class PageCell: UICollectionViewCell {
         imageView.anchorToTop(topAnchor, left: leftAnchor, bottom: textView.topAnchor, right: rightAnchor)
         textView.anchorWithConstantsToTop(nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 16, bottomConstant: 0, rightConstant: 16)
         lineSeparatorView.anchorToTop(nil, left: leftAnchor, bottom: textView.topAnchor, right: rightAnchor)
-        lineSeparatorView.heightAnchor.constraintEqualToConstant(1).active = true
-        textView.heightAnchor.constraintEqualToAnchor(heightAnchor, multiplier: 0.3).active = true
+        lineSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        textView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
     }
     
     
